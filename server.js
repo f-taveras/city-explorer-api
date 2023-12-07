006ff315762d98b297261ab85057083b3277773b
Felix A. Taveras <felixataveras1@gmail.com>
2023-12-07 14:10:11 -0500

    donde server settings

--- server.js
+++ server.js
@@ -10,7 +10,12 @@ const app = express();
 
 app.use(cors());
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (request, response) => {
+    let data = {message: "hello world"};
+    response.json(data)
+})
 
 app.listen(
     PORT,
